8de5426d882f403a6de73741e346c15eebe565e4
Joel Quanbeck <jqquanbeck@gmail.com>
2019-07-28 15:16:06 -0500

    Fix connection issues

--- runInterface.sh
+++ runInterface.sh
@@ -1,10 +1,6 @@
-# Set up CAN communication
-sudo ifconfig can0 down
-./canableStart.sh
-
 # Set up ROS parameters
 source ~/catkin_ws/devel/setup.bash
-export ROS_MASTER=http://192.168.1.100:11311/
+export ROS_MASTER_URI=http://192.168.1.100:11311/
 export ROS_HOSTNAME=192.168.1.101
 export ROS_IP=192.168.1.101
 
