ea5331e22b90d3874f2e880fb9f8d4122d4d44b8
Joel Kocer <joel.kocer@jacks.sdstate.edu>
2019-08-03 23:24:12 -0500

    Added ability to move vertical cursor, added horizontal constants

--- scripts/WebCamViewer.py
+++ scripts/WebCamViewer.py
@@ -6,52 +6,53 @@ import cv2
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from std_msgs.msg import String
+from std_msgs.msg import Float64
 import numpy as np
+import constant
 
 class webcam:
+
     def __init__(self):
+        global cusorAdjustment
         print("Initializing")
-        #self.image_pub = rospy.Publisher("crosshair_image", Image, queue_size=10)
         self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber("cv_camera/image_raw", Image, self.callback)
+        cusorAdjustment = 0
+        rospy.Subscriber("cv_camera/image_raw", Image, self.callback)
+        rospy.Subscriber("cursor_adjustmant", Float64, self.cursor_callback)
 
     def callback(self, data):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+            imageProcessing(image)
         except CvBridgeError as e:
             print(e)
 
-        #scale_percent = 100
-        #width = int(cv_image.shape[1] * scale_percent / 100)
-        #height = int(cv_image.shape[0] * scale_percent / 100)
-        #dim = (width, height)
+    def cursor_callback(self, msg):
+        global cusorAdjustment 
+        cusorAdjustment += int(msg.data)
+        print(cusorAdjustment)
 
-        cv_image = rotateImage(cv_image, 90)
+def imageProcessing(image):
+
+        global cusorAdjustment
+
+        cv_image = rotateImage(image, 90)
 
         height, width = cv_image.shape[:2]
 
-        print('Height: ', height, 'Width: ', width)
+        #print('Height: ', height, 'Width: ', width)
 
-        cv2.line(cv_image, (width/2,0), (width/2, height), (0, 0, 0), 3)    
+        cv2.line(cv_image, ((width/2) + constant.ADJUST_CENTER_LINE, 0), ((width/2) + constant.ADJUST_CENTER_LINE, height), (0, 0, 0), 3)    
+        cv2.line(cv_image, (0, (height/2) + cusorAdjustment), (width, (height/2) + cusorAdjustment), (0, 0, 0), 3)    
 
-        #cv_image = cv2.resize(cv_image, dim, interpolation = cv2.INTER_AREA)
-
+ 
         cv2.namedWindow("Image window", cv2.WINDOW_NORMAL)
         cv2.resizeWindow("Image window", width, height)
-        #dim = (int(abs(windowWidth)), int(abs(windowHeight)))
-        #resized = cv2.resize(cv_image, dim, interpolation = cv2.INTER_AREA)
-        #windowDetails = cv2.getWindowProperty("Image window")
-        #print(windowDetails)
+
         cv2.imshow("Image window", cv_image)
         cv2.waitKey(1)
 
 def rotateImage(image, angle):
-        #image_center = tuple(np.array(image.shape[1::-1]) / 2)
-        #rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-        #result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-        #rows,cols = image.shape
-        #M = cv2.getRotationMatrix2D((cols/2, rows/2), angle, 1)
-        #dst = cv2.warpAffine(image,M,(cols,rows))
 
         height, width = image.shape[:2]
         image_center = (width/2, height/2)
