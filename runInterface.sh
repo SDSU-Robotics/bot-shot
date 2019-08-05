2e70853b90814dafac15da64951390320133823c
Joel Quanbeck <jqquanbeck@gmail.com>
2019-08-04 20:50:19 -0500

    Kill more pythons

--- runInterface.sh
+++ runInterface.sh
@@ -5,12 +5,12 @@ export ROS_HOSTNAME=192.168.1.101
 export ROS_IP=192.168.1.101
 
 # Run the necessary nodes
-killall -9 WebCamViewer.py
+killall -9 joy_node
+killall -9 python
 sleep 2
 rosrun joy joy_node &
 rosrun bot-shot WebCamViewer.py &
 rosrun bot-shot PhysicsModel.py &
 rosrun bot-shot Interface
-
 wait
-
+killall -9 python
